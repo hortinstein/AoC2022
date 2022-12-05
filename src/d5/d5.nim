16e3a1226ee182d661b74c52ac5d74cd09e773d9
Alex Hortin <hortinstein@gmail.com>
2022-12-05 15:04:30 -0500

    Update d5.nim

--- src/d5/d5.nim
+++ src/d5/d5.nim
@@ -6,7 +6,7 @@ import std/algorithm
 
 #const input = staticRead("p1input")
 #https://stackoverflow.com/questions/30298950/initialize-a-seq-of-seqs
-const input= """[D]    
+const input= """    [D]    
 [N] [C]    
 [Z] [M] [P]
  1   2   3 
@@ -53,17 +53,24 @@ while j>0:
     curTower+=1
 #echo towers
 
-i+=1
+i+=1 #instruction number
 while i < lines.len:
   let instruction= lines[i].split()
-  let number = instruction[1]
-  let origin = instruction[3]
-  let dest = instruction[5]
+  let number =parseInt(instruction[1])
+  let origin =parseInt(instruction[3])
+  let dest = parseInt(instruction[5])
   echo "moving ", number, " boxes from ", origin, " to ", dest
+  j=0 #box counter
+  echo towers
+  while j < number:
+    let move = towers[origin-1].pop()
+    towers[dest-1].add(move)
+    echo towers
+    j+=1
   i+=1
 
-
-
-
-
-
+var topString = ""
+for tower in towers:
+  topString.add( $tower[^1])
+  
+echo topString
