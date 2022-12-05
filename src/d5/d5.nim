7d35928b021d00ef4dd157a5a115a34b87821431
Alex Hortin <hortinstein@gmail.com>
2022-12-05 14:30:37 -0500

    Update d5.nim

--- src/d5/d5.nim
+++ src/d5/d5.nim
@@ -16,18 +16,48 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2"""
   
+#finds the index of the line where instructions start
 var i = 0
 let lines = splitLines(input)
 while i< lines.len-1:
   if lines[i].len == 0:
     break
   i+=1
+  
+#gets the number of bins.
 let numBins =parseInt( max(lines[i-1].split(" ")))
 echo numBins
 
-var towers= seq[seq[char]]
+#creates a sequence of sequences
+var towers = newSeq[seq[char]]()
 
-for i2 in 0..numBins:
-  towers.insert(newSeq())
+#creates a new seq for each tower in the input
+for tower in 0..numBins-1:
+  var row = newSeq[char]()
+  towers.insert(row)
   
+#translates the ascii diagram into data structure representation....messily
+var curTower = 0
+var j=i-1
+while j>0:
+  echo lines[j]
+  j-=1
+  var k = 0
+  var curTower =0
+  while k+2<lines[j].len:
+    echo lines[j][k..k+2], "*"
+    
+    if lines[j][k+1] != ' ':
+      towers[curTower].add(lines[j][k+1])
+    k+=4
+    curTower+=1
+echo towers
+
+
+
+
+
+
+
+
 
