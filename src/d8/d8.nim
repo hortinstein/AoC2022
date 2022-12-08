5f36c5163902c0dfadb4edb676b5401622b87266
Alex Hortin <hortinstein@gmail.com>
2022-12-08 19:06:41 +0000

    p1 completed

--- src/d8/d8.nim
+++ src/d8/d8.nim
@@ -4,6 +4,14 @@ import std/sets
 import std/algorithm
 import "../util"
 
+proc green*(s: string): string = "\e[32m" & s & "\e[0m"
+proc grey*(s: string): string = "\e[90m" & s & "\e[0m"
+proc yellow*(s: string): string = "\e[33m" & s & "\e[0m"
+proc red*(s: string): string = "\e[31m" & s & "\e[0m"
+
+proc maxInSeq(s:seq[int]):int = 
+  return s[maxIndex(s)]
+
 const input = staticRead("p1input")
 let lines = splitLines(input)
 var trees = newSeq[seq[int]]()
@@ -17,29 +25,49 @@ for line in lines:
     row.add(heightInt)
   trees.add(row)    
 
-var visible = newSeq[seq[char]]()
-
 proc checkVis(trees: seq[seq[int]],x: int, y: int): bool=
   let checkHeight = trees[y][x] #rows/columns
-  if (x == 0 or y == 0): #check edge
+  #echo red("new item checked: "& $checkHeight)  
+  if (x == 0 or y == 0 or x == trees[0].len-1 or y == trees.len-1): #check edge
+    #echo "corner/edge"
     return true
-  let maxCol = maxIndex(trees[y])
-  if checkHeight < trees[y][maxCol] and maxCol != x: #check left
+  #echo trees[y][0..x-1], "left"
+  let lMax = maxInSeq(trees[y][0..x-1])
+  #echo trees[y][x+1..^1], "right"
+  let rMax = maxInSeq(trees[y][x+1..^1])
+  if checkHeight > lMax: #check left
+    #echo "left", lMax
+    return true
+  if checkHeight > rMax: #check right
+    #echo "right", rMax
     return true
   var col = newSeq[int]()
   for i in trees: 
     col.add(i[x])
-  let maxRow = maxIndex(col)
-  if checkHeight < col[maxRow] and maxRow != y: #check left
+  let uMax = maxInSeq(col[0..y-1])
+  #echo col[0..y-1], "up"
+  let dMax = maxInSeq(col[y+1..^1])
+  #echo col[y+1..^1],"down"
+  if checkHeight > uMax: #check up
+    #echo "up", uMax
+    return true
+  if checkHeight > dMax: #check down
+    #echo "down", dMax
     return true
   return false
+
+
+var visString = "" 
 
 var total_vis = 0
 for y in 0..trees.len-1:
   for x in 0..trees[0].len-1:
     if checkVis(trees,x,y):
       total_vis+=1
+      visString.add( green($trees[y][x]) )
+    else:
+      visString.add( grey($trees[y][x]) )
+  visString.add("\n")
 
-
-
+echo visString
 echo total_vis
